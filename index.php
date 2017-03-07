5a7e91455a56de651e58895bc42441f5e4d54f5b
hasna Chaddou <hasnachad@gmail.com>
2017-03-07 14:00:47 +0100

    exo php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$nom = "chaddou";
+$prenom = "hasna";
+$age = 35;
+$kilometre = 125;
+
+echo $nom;
+echo $prenom;
+echo $age;
+echo $kilometre;
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/css/style.css">
+</head>
+
+<body>
+
+
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,8 @@
+1 - lien variable.php link
+nom du lien exercixe 1
+2 - var nom iniatliser avec valeur
+	afficher contenu
+	creer 3 var nom prenom age
+
+3 - km initalise a 1
+afficher contenue
